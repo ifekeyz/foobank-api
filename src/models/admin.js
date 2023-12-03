f17fa2e24e09c95083eff012ed9714513067f52c
Oluwaferanmi Micheal <68645366+ifekeyz@users.noreply.github.com>
2023-12-03 19:01:56 +0100

    Update admin.js

--- src/models/admin.js
+++ src/models/admin.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const adminVatSchema = new mongoose.Schema({
   fullName: { type: String, required: true },
   vat: { type: Number, required: true },
+  interest: { type: Number },
   serviceFee: { type: Number, required: true },
   deliveryFee:{type:Number, required:true},
   date: { type: Date, default: Date.now },
