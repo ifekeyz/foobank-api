ddef6c3a2dc68243f9da475db0f08a8e24f4e6d0
unknown <you@example.com>
2023-12-12 16:48:29 +0100

    update app.js

--- src/helpers/jwt.js
+++ src/helpers/jwt.js
@@ -12,13 +12,11 @@ function authJwt(){
     }).unless({
         path:[
             `/`,
-            `/public/uploads/(.*)`,
             `${api}/users/login`,
             `${api}/users/register`,
             `${api}/users/(.*)`,
             `${api}/users/change/password/(.*)`,
             `${api}/user/verify-code`,
-            {url:/\/public\/uploads(.*)/,methods:['GET','OPTIONS']},
             {url:/\/api\/v1\/users\/change\/password(.*)/,methods:['POST','OPTIONS']},
             {url:/\/api\/v1\/users(.*)/,methods:['GET','OPTIONS']},
             {url:/\/api\/v1\/users(.*)/,methods:['POST','OPTIONS']},
