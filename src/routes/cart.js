64d5142aa367caf0fc0182ff9da706e71b975476
unknown <you@example.com>
2023-12-15 10:37:47 +0100

    new load push

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -43,7 +43,7 @@ router.delete('/deleteCartItem/:userId/:itemId', async (req, res) => {
         // Find the index of the cart item to remove
         const itemIndex = cart.items.findIndex(item => item._id === itemId);
 
-        if (itemIndex === 0) {
+        if (itemIndex < 0) {
             return res.status(404).json({ message: "Item not found in the cart" });
         }
 
@@ -60,6 +60,40 @@ router.delete('/deleteCartItem/:userId/:itemId', async (req, res) => {
         res.status(500).json({ message: 'Error deleting cart item', error: error.message });
     }
 });
+
+
+// router.delete('/deleteCartItem/:userId/:itemId', async (req, res) => {
+//     const userId = req.params.userId;
+//     const itemId = req.params.itemId;
+
+//     try {
+//         // Find the cart document for the specified user
+//         const cart = await Cart.findOne({ userId });
+
+//         if (!cart) {
+//             return res.status(404).json({ message: "Opps! Cart not found " });
+//         }
+
+//         // Find the index of the cart item to remove
+//         const itemIndex = cart.items.findIndex(item => item._id === itemId);
+
+//         if (itemIndex === 0) {
+//             return res.status(404).json({ message: "Item not found in the cart" });
+//         }
+
+//         // Remove the item from the cart
+//         cart.items.splice(itemIndex, 1);
+
+//         // Save the updated cart document
+//         await cart.save();
+
+//         // Respond with a success message or updated cart data
+//         return res.status(200).json({ message: "Cart item deleted successfully", updatedCart: cart });
+//     } catch (error) {
+//         console.error('Error deleting cart item:', error);
+//         res.status(500).json({ message: 'Error deleting cart item', error: error.message });
+//     }
+// });
 
 
 module.exports = router;
